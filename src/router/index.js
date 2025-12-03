e9bec9885a46558672fa66bf7ba3e651ad65846c
杨雯 <136423275@qq.com>
2025-12-03 17:34:56 +0800

    部署

--- src/router/index.js
+++ src/router/index.js
@@ -139,6 +139,7 @@ export const constantRoutes = [
 
 export default new Router({
   mode: 'history', // 去掉url中的#
+  base:process.env.NODE_ENV === 'production' ? '/ljfl/' : '/',
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
