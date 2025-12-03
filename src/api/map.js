1f16e1a58f95ad7eceebc553055e6e4890054022
杨雯 <136423275@qq.com>
2025-12-03 17:08:34 +0800

    Updates

--- src/api/map.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-
-export function getGeoJson(api) {
-    return request({
-      url: api,
-      method: 'get'
-    })
-  }
--- src/views/export/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>导出</div>
-</template>
-<script>
-export default {
-    
-}
-</script>
